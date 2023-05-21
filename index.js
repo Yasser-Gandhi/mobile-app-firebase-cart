fd8a4ddfc8dd9d2fc1d62c469d9702cc7b8f2200
Gandhi Hernández <79735730+Yasser-Gandhi@users.noreply.github.com>
2023-05-20 20:50:44 -0600

    Actualizado

--- index.js
+++ index.js
@@ -6,67 +6,61 @@ const appSettings = {
     databaseURL: "https://realtime-database-3eba5-default-rtdb.firebaseio.com/"
 }
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const shoppingListInDB = ref(database, "shoppingList");
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
-const inputFieldEl = document.getElementById("input-field");
-const addButtonEl = document.getElementById("add-button");
-const shoppingListEl = document.getElementById("shopping-list");
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value;
-
-    push(shoppingListInDB, inputValue);
-
-    clearInputFieldEl();
-});
-
-// Agrega un evento 'input' al campo de entrada para habilitar o deshabilitar el botón
-inputFieldEl.addEventListener("input", function() {
-    addButtonEl.disabled = inputFieldEl.value === "";
-});
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
 
 onValue(shoppingListInDB, function(snapshot) {
     if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val());
-
-        clearShoppingListEl();
-
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl()
+        
         for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i];
-            let currentItemID = currentItem[0];
-            let currentItemValue = currentItem[1];
-
-            appendItemToShoppingListEl(currentItem);
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
         }    
     } else {
-        shoppingListEl.innerHTML = "Nada que comprar :)";
+        shoppingListEl.innerHTML = "No items here... yet"
     }
-});
+})
 
 function clearShoppingListEl() {
-    shoppingListEl.innerHTML = "";
+    shoppingListEl.innerHTML = ""
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = "";
-    addButtonEl.disabled = true; // Deshabilita el botón al borrar el campo de entrada
+    inputFieldEl.value = ""
 }
 
 function appendItemToShoppingListEl(item) {
-    let itemID = item[0];
-    let itemValue = item[1];
-
-    let newEl = document.createElement("li");
-
-    newEl.textContent = itemValue;
-
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
     newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
-
-        remove(exactLocationOfItemInDB);
-    });
-
-    shoppingListEl.append(newEl);
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingListEl.append(newEl)
 }
