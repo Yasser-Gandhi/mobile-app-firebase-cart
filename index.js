a4a12bc9f694d8694066510d5eac945c08a6022d
Gandhi Hernández <79735730+Yasser-Gandhi@users.noreply.github.com>
2023-05-20 20:48:14 -0600

    Boton deshabilitar

--- index.js
+++ index.js
@@ -6,59 +6,67 @@ const appSettings = {
     databaseURL: "https://realtime-database-3eba5-default-rtdb.firebaseio.com/"
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue)
-    
-    clearInputFieldEl()
-})
+    let inputValue = inputFieldEl.value;
+
+    push(shoppingListInDB, inputValue);
+
+    clearInputFieldEl();
+});
+
+// Agrega un evento 'input' al campo de entrada para habilitar o deshabilitar el botón
+inputFieldEl.addEventListener("input", function() {
+    addButtonEl.disabled = inputFieldEl.value === "";
+});
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.entries(snapshot.val())
-    
-    clearShoppingListEl()
-    
-    for (let i = 0; i < itemsArray.length; i++) {
-        let currentItem = itemsArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        
-        appendItemToShoppingListEl(currentItem)
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
+
+        clearShoppingListEl();
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i];
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1];
+
+            appendItemToShoppingListEl(currentItem);
+        }    
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet";
     }
-})
+});
 
 function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
+    shoppingListEl.innerHTML = "";
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+    inputFieldEl.value = "";
+    addButtonEl.disabled = true; // Deshabilita el botón al borrar el campo de entrada
 }
 
 function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = itemValue
-    
-    // Challenge: Attach an event listener to newEl and make it so you console log the id of the item when it's pressed.
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    let newEl = document.createElement("li");
+
+    newEl.textContent = itemValue;
+
     newEl.addEventListener("click", function() {
-        // Challenge: Make a let variable called 'exactLocationOfItemInDB' and set it equal to ref(database, something) where you substitute something with the code that will give you the exact location of the item in question.
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        remove(exactLocationOfItemInDB)
-        // Challenge: Use the remove function to remove the item from the database
-    })
-    
-    shoppingListEl.append(newEl)
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+
+        remove(exactLocationOfItemInDB);
+    });
+
+    shoppingListEl.append(newEl);
 }
